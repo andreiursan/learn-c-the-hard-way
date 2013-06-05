f3d77acbae5fc24ad7f155908e98d96c4ae7d27b
Andrei Ursan <hello@andreiursan.com>
2013-06-05 17:42:53 +0200

    ex02 The Makefile

---
+++ ex02/ex01.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[]){
+  puts("Extra\n\tCredit\nis easy.");
+  puts("If you delete random things in the exec file:"); 
+  puts("zsh: malformed Mach-o file: ./ex01");
+  
+  return 0;
+}
