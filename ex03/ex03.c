f1e46733e1f29d14c70069da23f19793a35aceab
Andrei Ursan <hello@andreiursan.com>
2013-06-08 12:33:16 +0200

    ex03, some extra tall credit

--- ex03/ex03.c
+++ ex03/ex03.c
@@ -3,7 +3,11 @@
 int main(){
   int age = 10;
   int height = 72;
+  float johnHeight = 71.7778;
 
   printf("I am %d years old.\n", age);
   printf("I am %d inches tall.\n", height);
+  printf("\vJohn is %f inches.\n", johnHeight); 
+  printf("But you can round it to %.2f inches.\n", johnHeight);
+  printf("Or you can trunc it to %g inches.\n", johnHeight);
 }
